7dfe20cb8cc7f813c6ae05388dd645fa5be48052
afisyanasywa <afisyaanasywa@gmail.com>
2026-04-27 23:42:31 +0700

    pertemuan 8 week 9 (asynchronous)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,3 +25,12 @@ Route::get('pert5', function () {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class,'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
