af619ab114f287fc6e6ff73c0253134160e264f5
afisyanasywa <afisyaanasywa@gmail.com>
2026-05-25 12:12:02 +0700

    pertemuan 12

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,39 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('template')
+@section('title', 'Data Pegawai')
+@section('konten')
 
-<head>
-    <title>5026241035</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
+	<a href="/pegawai/tambah" class="btn btn-primary">Tambah Pegawai Baru</a>
 
-<body>
-    <div class="container">
-        <center>
-            <h1>5026241035 Afisya Nasywa Tsabitah</h1>
-            <p>
-                <a href="pertemuan5.html" target="_blank" class="btn btn-primary">
-                    Pertemuan 5</a>
-            </p>
-            <p>
-                <a href="linktree.html" target="_blank" class="btn btn-primary">
-                    Tugas Link Tree</a>
-            </p>
-            <p>
-                <a href="" target="_blank" class="btn btn-primary">
-                    Tugas Make Over Akun Instagram </a>
-            </p>
-            <p>
-                <a href="" target="_blank" class="btn btn-primary">
-                    UTS</a>
-            </p>
-        </center>
+	<br/>
+	<br/>
 
-    </div>
-</body>
+    <p>Cari Data Pegawai :</p>
+	<form action="/pegawai/cari" method="GET" class="form-inline">
+        <div class="form-group">
+		<input type="text" name="cari" placeholder="Cari Pegawai .." class="form-control">
+		<input type="submit" value="CARI" class="btn btn-light">
+        </div>
+	</form>
 
-</html>
+	<br/>
+
+	<table class="table table-striped table-hover">
+		<tr>
+			<th>Nama</th>
+			<th>Jabatan</th>
+			<th>Umur</th>
+			<th>Alamat</th>
+			<th>Opsi</th>
+		</tr>
+		@foreach($pegawai as $p)
+		<tr>
+			<td>{{ $p->pegawai_nama }}</td>
+			<td>{{ $p->pegawai_jabatan }}</td>
+			<td>{{ $p->pegawai_umur }}</td>
+			<td>{{ $p->pegawai_alamat }}</td>
+			<td>
+				<a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
+
+				<a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
+			</td>
+		</tr>
+		@endforeach
+	</table>
+    {{$pegawai->links()}}
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,15 @@ Route::get('pert5', function () {
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
 
+//crud tabel pegawai
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::get('/pegawai/store', [PegawaiDBController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::get('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
+
 Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class,'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class,'proses']);
@@ -35,8 +44,6 @@ Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
 
-//crud tabel pegawai
-Route::get('/pegawai', [PegawaiDBController::class, 'index']);
 
 // route pertemuan 1-6
 Route::get('nrp', function () {
